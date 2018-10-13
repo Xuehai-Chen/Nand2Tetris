c369bb5d2cd042494d665d8f2e13e86f2f26d300
Xuehai-Chen <13162576651@163.com>
2018-10-13 16:44:52 +0800

    complete symbolTable & codeWriter

--- projects/11/JackCompiler/src/CodeWriter.java
+++ projects/11/JackCompiler/src/CodeWriter.java
@@ -11,39 +11,48 @@ public class CodeWriter {
     }
 
     public void writePush(String segment, int index) throws Exception {
-
+        writer.write("push " + segment + " " + index);
+        writer.newLine();
     }
 
     public void writePop(String segment, int index) throws Exception {
-
+        writer.write("pop " + segment + " " + index);
+        writer.newLine();
     }
 
     public void writeArithmetic(String command) throws Exception {
-
+        writer.write(command);
+        writer.newLine();
     }
 
     public void writeLabel(String label) throws Exception {
-
+        writer.write("(" + label + ")");
+        writer.newLine();
     }
 
     public void writeGoto(String label) throws Exception {
-
+        writer.write("goto " + label);
+        writer.newLine();
     }
 
     public void writeIf(String label) throws Exception {
-
+        writer.write("if-goto " + label);
+        writer.newLine();
     }
 
     public void writeCall(String label) throws Exception {
-
+        writer.write("call " + label);
+        writer.newLine();
     }
 
     public void writeFunction(String label) throws Exception {
-
+        writer.write("function " + label);
+        writer.newLine();
     }
 
     public void writeReturn() throws Exception {
-
+        writer.write("return");
+        writer.newLine();
     }
 
     public void close() throws Exception {
--- projects/11/JackCompiler/src/SymbolTable.java
+++ projects/11/JackCompiler/src/SymbolTable.java
@@ -1,35 +1,93 @@
+import java.util.HashMap;
 
 public class SymbolTable {
 
+    private HashMap<String, Object[]> classSymbolTable;
+    private HashMap<String, Object[]> subroutineSymbolTable;
+    private int staticCount = 0;
+    private int fieldCount = 0;
+    private int argCount = 0;
+    private int varCount = 0;
+
     public enum IdentifierKind {
-        STATIC, FIELD, ARG, VAR
+        STATIC, FIELD, ARG, VAR, UNDEFINED
     }
 
     public SymbolTable() {
-
+        classSymbolTable = new HashMap<>();
+        subroutineSymbolTable = new HashMap<>();
     }
 
     public void startSubroutine() {
-
+        subroutineSymbolTable = new HashMap<>();
+        argCount = 0;
+        varCount = 0;
     }
 
     public void define(String name, String type, IdentifierKind kind) {
-
+        if (kind == IdentifierKind.STATIC) {
+            Object[] entity = {type, kind, staticCount};
+            classSymbolTable.put(name, entity);
+            staticCount++;
+        } else if (kind == IdentifierKind.FIELD) {
+            Object[] entity = {type, kind, fieldCount};
+            classSymbolTable.put(name, entity);
+            fieldCount++;
+        } else if (kind == IdentifierKind.ARG) {
+            Object[] entity = {type, kind, argCount};
+            subroutineSymbolTable.put(name, entity);
+            argCount++;
+        } else if (kind == IdentifierKind.VAR) {
+            Object[] entity = {type, kind, varCount};
+            subroutineSymbolTable.put(name, entity);
+            varCount++;
+        } else {
+            System.out.println("The identifier kind is undefined.");
+        }
     }
 
     public int varCount(IdentifierKind kind) {
-        return 0;
+        switch (kind) {
+            case STATIC:
+                return staticCount;
+            case FIELD:
+                return fieldCount;
+            case ARG:
+                return argCount;
+            case VAR:
+                return varCount;
+            default:
+                return 0;
+        }
     }
 
     public IdentifierKind kindOf(String name) {
-        return IdentifierKind.ARG;
+        if (subroutineSymbolTable.containsKey(name)) {
+            return (IdentifierKind) subroutineSymbolTable.get(name)[1];
+        } else if (classSymbolTable.containsKey(name)) {
+            return (IdentifierKind) classSymbolTable.get(name)[1];
+        }
+        System.out.println("The identifier is undefined.");
+        return IdentifierKind.UNDEFINED;
     }
 
     public String typeOf(String name) {
-        return "int";
+        if (subroutineSymbolTable.containsKey(name)) {
+            return (String) subroutineSymbolTable.get(name)[0];
+        } else if (classSymbolTable.containsKey(name)) {
+            return (String) classSymbolTable.get(name)[0];
+        }
+        System.out.println("The identifier is undefined.");
+        return "UNDEFINED";
     }
 
     public int indexOf(String name) {
+        if (subroutineSymbolTable.containsKey(name)) {
+            return (int) subroutineSymbolTable.get(name)[2];
+        } else if (classSymbolTable.containsKey(name)) {
+            return (int) classSymbolTable.get(name)[2];
+        }
+        System.out.println("The identifier is undefined.");
         return 0;
     }
 }
